4ca34187795f4a61beff02a25cef7df8159e61fc
cem özkan <cemozkan9135@gmail.com>
2017-08-06 20:33:11 +0300

    ikinci commit
    db edit

--- panel/addproduct.php
+++ panel/addproduct.php
@@ -1,6 +1,41 @@
 <?php 
+require_once("inc/newconfig.php");
 
+function addDb (){
+	$title=$_POST['title'];
+	$desc=$_POST['desc'];
+	$price=$_POST['price'];
+	$size= $_POST['size'];
+	
+ /*if (isset($_POST['submit'])) { //to check if the form was submitted
+       
+       $db->exec( 'INSERT INTO panel (id, title, description, price, size) VALUES (NULL, '.$_POST['title'].', '.$_POST['desc'].', '.$_POST['price'].', '.$_POST['size'].');');
+       echo "a";
+       echo 'INSERT INTO panel (id, title, description, price, size) VALUES (NULL, '.$_POST['title'].', '.$_POST['desc'].', '.$_POST['price'].', '.$_POST['size'].');';
+    } */
+}
+if($_POST){
+    $title=$_POST['title'];
+	$desc=$_POST['desc'];
+	$price=$_POST['price'];
+	$size= $_POST['size'];
+  $insert = $db->query("INSERT INTO panel ( title, description, price, size) VALUES ('$title', '$desc', '$price','$size')");
+  if($insert)
+  {
+    $message = "User added successfully!";
+	echo "asda";
+	}
+  else
+  {
+	echo "else";
+    $error = "There was a database error.";
+}
+
+}
 ?>
+
+
+
 	<!DOCTYPE html>
 	<html>
 	<head>
@@ -20,20 +55,20 @@
  <div class="x_content">
                   <form method="post" enctype="multipart/form-data">
                     <label for="title">Title :</label>
-                    <input type="text" name="title" id="title" class="form-control">
+                    <input type="text" name="title" id="title" class="form-control"><br>
 
                     <label for="desc">Description * :</label>
-                    <input type="text" name="desc" id="desc" class="form-control">
+                    <input type="text" name="desc" id="desc" class="form-control"><br>
 
                     <label for="price">Price * :</label>
-                    <input type="text" name="price" id="price" class="form-control">
+                    <input type="text" name="price" id="price" class="form-control"><br>
 
                     <label for="size">Size * :</label>
-                     <input type="text" name="size" id="size" class="form-control">
+                     <input type="text" name="size" id="size" class="form-control"><br>
 
                     <div class="ln_solid"></div>
 
-                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="submit"  class="btn btn-primary">Save</button>
 
                   </form>
                   <!-- end form for validations -->
